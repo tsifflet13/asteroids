ca348e4a14023f1537d9d2357105052d568af5da
Hikiyo <thomas.sifflet@gmail.com>
2025-12-30 22:40:03 +0100

    C: add player movement

--- main.py
+++ main.py
@@ -8,20 +8,23 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+    
     while True:
-        clock = pygame.time.Clock()
-        dt = 0
+        dt = clock.tick(60) / 1000
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
 
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
+
 
         
 
