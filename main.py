ccdebbf485f9443178873727a43941f33c209e5e
Hikiyo <thomas.sifflet@gmail.com>
2025-12-31 12:04:08 +0100

    E: add collision with asteroids and game over

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+import sys
 
 def main():
     pygame.init()
@@ -33,6 +34,11 @@ def main():
 
 
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         for to_draw in drawable:
             to_draw.draw(screen)
         pygame.display.flip()
